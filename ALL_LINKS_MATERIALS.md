6411ae777d90243bc969e537bbe38f36a6be0442
scapegoatdekma <48290767+scapegoatdekma@users.noreply.github.com>
2025-02-06 23:53:35 +0300

    Initial commit

---
+++ ALL_LINKS_MATERIALS.md
@@ -0,0 +1,98 @@
+ALL_BY_IT_THEMES:
+  - https://www.youtube.com/@ListenIT_channel  # хороший канал с качественным материалом
+  - https://www.youtube.com/@codefestru/videos
+  - https://www.youtube.com/@AlekOS
+  - https://www.youtube.com/@merionacademy
+  - https://www.youtube.com/@t0digital
+  - https://www.youtube.com/@CompscicenterRu
+  - https://www.youtube.com/@NerzonIT
+  - https://www.youtube.com/@svyatIT/videos
+  - https://www.youtube.com/@ivan.goncharov/playlists
+  - https://www.youtube.com/@digital_ninja/playlists
+  - https://www.youtube.com/@lessonfirst/videos  # OLD GUIDS FOR PHP, SQL, JAVA ....
+  - https://www.youtube.com/@itgid/videos  # FRONTEND
+  - https://www.youtube.com/@studentsdaysit2002/playlists # math, py, cisco, сети
+
+DEPLOY:
+  - https://www.youtube.com/@cyber-paddle/playlists # +РАЗРАБОТКА
+
+PODCASTS:
+  - https://www.youtube.com/@ProBelDev
+
+SBER_SQL:
+  - https://gitlab.com/school218063421/sql-projects/s21-sql-bootcamp
+
+freecodecamp:
+  - https://www.freecodecamp.org/
+
+w3schools:
+  - https://www.w3schools.com/sql/
+
+ENGLISH_SQL:
+  - https://www.mysqltutorial.org
+
+RUSSIAN_SQL:
+  - https://sql-academy.org
+
+STEPIK_SQL:
+  - https://stepik.org/course/63054/
+
+VIDEOS:
+  - https://openedu.ru/course/spbu/DTBS/
+  - https://www.youtube.com/@t0digital
+
+YANDEX:
+  - https://www.youtube.com/watch?v=YjSIdz8DnAo
+  - https://www.youtube.com/@CompscicenterRu
+
+HACKER:
+  - https://www.youtube.com/@abrictosecurity
+  -
+University:
+  - https://www.youtube.com/@CompscicenterRu/playlists #множество курсов связанных с университетскими направлениями
+backend:
+  PYTHON:
+    - https://www.youtube.com/@NeuralNine # PY TUTORIALS NeuralNine
+    - https://www.youtube.com/@PythonRussian # Python Russian
+    - https://www.youtube.com/@ITPro-ei8cs   # алгоритмы
+    - https://www.youtube.com/@IT4eachcom    # множество по py, linux, ubuntu
+    - https://www.youtube.com/@selfedu_rus  # selfedu
+    - https://www.youtube.com/@BroCodez     # many tutorials ENGLISH
+    - https://www.youtube.com/@PythononPapyrusRU
+    - https://www.youtube.com/@pymentor
+    - https://www.youtube.com/@pythonclinic
+    - https://www.youtube.com/@t0digital
+    - https://www.youtube.com/@coreyms   # TUTORIALS PY
+    - https://www.youtube.com/@PythonToday
+    - https://www.youtube.com/@freecodecamp  # freecodecamp
+  GO:
+    - https://www.youtube.com/@nikolay_tuzov
+  ASSEMBLY_AND_C:
+    - https://www.youtube.com/@firststepsforward/playlists
+
+frontend:
+  - https://www.youtube.com/@UlbiTV
+  - https://www.youtube.com/@itgid/videos
+
+Хириянов:
+  - https://www.youtube.com/@tkhirianov
+
+UNICODE:
+  - https://www.youtube.com/@Uni-Coder  # C, DATATYPES, уникальный канал с интересными видео + устройством компьютера
+
+devops:
+  - https://www.youtube.com/@ADV-IT
+
+MATH:
+  - https://www.youtube.com/@SergejKuts
+  - https://www.youtube.com/@mektep_online_matematika
+  - https://www.youtube.com/@matematiks/videos
+  - https://www.youtube.com/@da_vinci_center
+
+ENGLISH:
+  - https://www.youtube.com/@engvidEmma
+  - https://www.youtube.com/@ok-english
+  - https://www.youtube.com/@gabbywallaceofficial
+
+DATABASES_SETUPS:
+  - https://www.youtube.com/@IT-xk5lo
---
+++ gulpfile.js
@@ -0,0 +1,107 @@
+const gulp = require("gulp");
+const pug = require("gulp-pug");
+const sass = require("gulp-sass")(require("sass"));
+const cleanCSS = require("gulp-clean-css");
+const browserSync = require("browser-sync").create(); //  Для LiveReload
+const data = require("gulp-data"); // Для передачи данных в Pug
+const babel = require("gulp-babel"); // Для преобразования ES6+ в ES5 (используется для совместимости со старыми браузерами)
+const uglify = require("gulp-uglify"); // Для минификации (сжатия) JavaScript
+const sourcemaps = require("gulp-sourcemaps"); // Для генерации sourcemap
+const fs = require("fs");
+
+const paths = {
+  pug: {
+    src: "src/pug/*.pug",
+    dest: "dist",
+  },
+  sass: {
+    src: "src/sass/**/*.scss",
+    dest: "dist",
+  },
+  data: "./data/guides.json",
+  fonts: {
+    src: "node_modules/@fortawesome/fontawesome-free/webfonts/*.*",
+    dest: "dist/fonts/", // Destination for Font Awesome fonts
+  },
+  js: {
+    src: "src/js/*.js", // Путь к вашим исходным JavaScript файлам (например, src/js/main.js)
+    dest: "dist/js", // Папка, куда будут сохранены скомпилированные файлы (например, dist/js/)
+  },
+};
+
+function loadData() {
+  try {
+    const fileContent = fs.readFileSync(paths.data, "utf8");
+    const data = JSON.parse(fileContent); // Используем JSON.parse
+    return data;
+  } catch (error) {
+    console.error("Ошибка при загрузке данных:", error);
+    return {};
+  }
+}
+function compilePug() {
+  // compileSass();
+  return gulp
+    .src(paths.pug.src)
+    .pipe(
+      data(async function () {
+        return loadData();
+      })
+    ) // Вот здесь передаются данные
+    .pipe(pug({ pretty: true }))
+    .pipe(gulp.dest(paths.pug.dest))
+    .pipe(browserSync.stream());
+}
+function compileFonts() {
+  return gulp
+    .src(paths.fonts.src)
+    .pipe(gulp.dest(paths.fonts.dest))
+    .pipe(browserSync.stream());
+}
+function compileSass() {
+  return gulp
+    .src(paths.sass.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(cleanCSS({ compatibility: "ie8" })) // Сжимаем CSS (
+    .pipe(gulp.dest(paths.sass.dest))
+    .pipe(browserSync.stream()); // Обновляем браузер после компиляции
+}
+function compileJS() {
+  return gulp
+    .src(paths.js.src) // Получаем исходные файлы
+    .pipe(sourcemaps.init()) // Инициализируем генерацию sourcemap
+    .pipe(
+      babel({
+        // Преобразуем ES6+ код в ES5
+        presets: ["@babel/preset-env"],
+      })
+    )
+    .pipe(uglify()) // Минифицируем (сжимаем) JavaScript
+    .pipe(sourcemaps.write(".")) // Записываем sourcemap в ту же папку (или в отдельную, если указать путь)
+    .pipe(gulp.dest(paths.js.dest)); // Сохраняем скомпилированные файлы в папку назначения
+}
+
+// Задача для запуска BrowserSync
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: "dist",
+    },
+    notify: true,
+    open: true, // Не открывать браузер автоматически
+  });
+}
+
+function watchFiles() {
+  gulp.watch("src/pug/**/*.pug", compilePug); // Следим за файлами Pug
+  gulp.watch(paths.sass.src, compileSass); // Следим за файлами Sass
+  gulp.watch(paths.data, compilePug); // Следим за файлом данных (JSON)
+  gulp.watch(paths.fonts.src, compileFonts);
+  gulp.watch(paths.js.src, compileJS);
+}
+const build = gulp.series(compilePug, compileSass, compileFonts, compileJS);
+const watchTask = gulp.parallel(watchFiles, serve); // Запускаем watchFiles и serve параллельно
+
+// Экспорт задач
+exports.build = build;
+exports.default = gulp.series(build, watchTask); // Сначала build, потом watch
